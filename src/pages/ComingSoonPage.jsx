4cbb05d4665cb1b4912cb0dcfcb8922d67680ef7
bhagirath-bhp <bhagirath.bhp@gmail.com>
2024-01-17 06:24:39 +0530

    Update

--- src/pages/ComingSoonPage.jsx
+++ src/pages/ComingSoonPage.jsx
@@ -18,14 +18,14 @@ import '../styles/styles.css'
 const ComingSoonPage = () => {
   const [open, setOpen] = useState(false);
   const [hamMsg, setHamMsg] = useState("More Info")
-  const openDrawer = () => {setOpen(true); setHamMsg("Close")};
-  const closeDrawer = () => {setOpen(false); setHamMsg("More")};
+  const openDrawer = () => { setOpen(true); setHamMsg("Close") };
+  const closeDrawer = () => { setOpen(false); setHamMsg("More Info") };
   const handelDrawer = () => {
-    open?closeDrawer():openDrawer();
+    open ? closeDrawer() : openDrawer();
   }
   return (
     <div className="smMobile:px-1 tablet:p-5 flex justify-center flex-col bg-transparent">
-      <ParticlesLayer/>
+      <ParticlesLayer />
       <div className="cs-top flex justify-between w-full">
         <div className="nav">
           <Button onClick={handelDrawer} className="absolute left-[.5rem] z-[999999] tablet:top-[2rem] smMobile:top-[1rem] flex gap-3 text-xl bg-transparent border-[1px] border-gray-900 text-blue-600">
@@ -61,7 +61,7 @@ const ComingSoonPage = () => {
                     With an impeccable track record and experience of more than a decade, we stand as guardians of safety, offering a comprehensive suite of security solutions designed to protect you and what matters most to you. With an unwavering commitment to excellence and a dedication to staying ahead of emerging threats, we redefine the standards of security in an ever-evolving world.
                   </Typography>
                   <Typography color="gray" className="mb-8 pr-4 w-[75%] cs-content text-[18px] tracking-wide">
-                  By choosing MCCS, you're not just getting a one-size-fits-all solution - you're receiving a bespoke security framework crafted to address your distinct needs, ensuring optimal protection and peace of mind.
+                    By choosing MCCS, you're not just getting a one-size-fits-all solution - you're receiving a bespoke security framework crafted to address your distinct needs, ensuring optimal protection and peace of mind.
                   </Typography>
                 </div>
               </div>
@@ -122,24 +122,24 @@ const ComingSoonPage = () => {
         </div>
         <div className="logo bg-logo"></div>
       </div>
-      <div className="cs-main flex smMobile:flex-col tablet:flex-row justify-around items-center h-full w-full smMobile:p-0 tablet:px-[5rem]">
-        <div className="cs-countdown smMobile:hidden tablet:block relative">
+      <div className="cs-main flex smMobile:flex-col tablet:flex-row justify-around smMobile:items-center tablet:items-start h-full w-full smMobile:p-0 tablet:px-[5rem]">
+        <div className="cs-countdown smMobile:hidden tablet:block relative tablet:my-[5rem]">
           <div className="bar w-[2rem] h-[1px] bg-blue-600 absolute left-[-2.5rem] top-[.7rem]"></div>
           <p className="cs-head uppercase">
             LAUNCHING IN
           </p>
           <CustomCountdown days={2} hours={5} minutes={30} seconds={15} />
         </div>
-        <div className="cs-content tablet:max-w-[50%] smMobile:max-w-[80%] relative h-full">
-          <div className="bar w-[2rem] h-[1px] bg-blue-600 absolute left-[-2.5rem] top-[.7rem]"></div>
-          <p className="cs-head uppercase">
-            WELCOME TO MCCS
+        <div className="cs-content my-[5rem] tablet:max-w-[50%] smMobile:max-w-[80%] h-full">
+          <p className="cs-head uppercase ml-[10%] smMobile:my-[5rem] tablet:m-0 relative">
+            <div className="bar w-[2rem] h-[1px] bg-blue-600 absolute top-[50%] left-[-3rem]"></div>
+            <span>WELCOME TO MCCS</span>
           </p>
-          <p className="text-[3.5rem] text-white font-DomineRegular leading-[4rem] smMobile:text-center tablet:text-left">We are currently working on a new super awesome website.</p>
-          <p className="my-[3rem] cs-content smMobile:text-center tablet:text-left">At MCCS, transparency is not just a promise; it's a cornerstone of our service philosophy, reflecting our dedication to keeping you informed and in control of your security solutions.</p>
+          <p className="text-[3.5rem] text-white font-DomineRegular leading-[4rem] verySmMobile:text-center smMobile:text-left">We are currently working on a new super awesome website.</p>
+          <p className="my-[3rem] cs-content verySmMobile:text-center smMobile:text-left">At MCCS, transparency is not just a promise; it's a cornerstone of our service philosophy, reflecting our dedication to keeping you informed and in control of your security solutions.</p>
           <form className="subscribe-email">
-            <input type="email" className="px-[2rem] py-[1rem] text-white bg-[#ffffff20]" placeholder="Enter email" />
-            <button type="submit" className="px-[2rem] py-[1rem] bg-blue-600 text-white">Notify Me</button>
+            <input type="email" className="px-[2rem] py-[1rem] text-white bg-[#ffffff20] smMobile:w-full smMobile:my-[1rem] mobile:w-auto tablet:my-0" placeholder="Enter email" />
+            <button type="submit" className="px-[2rem] py-[1rem] bg-blue-600 text-white smMobile:w-full smMobile:my-[1rem] mobile:w-auto tablet:my-0">Notify Me</button>
           </form>
         </div>
         <div className="connect-socials flex flex-col gap-[2rem] smMobile:hidden tablet:flex absolute right-[1rem] top-[40%]">
@@ -164,8 +164,11 @@ const ComingSoonPage = () => {
             <p className="absolute right-[4rem] transition duration-300">Youtube</p>
           </div>
         </div>
-        <div className="cs-countdown tablet:hidden my-[5rem]">
-          Launching In
+        <div className="cs-countdown relative tablet:hidden">
+          <div className="bar w-[2rem] h-[1px] bg-blue-600 absolute left-[-2.5rem] top-[.7rem]"></div>
+          <p className="cs-head uppercase">
+            LAUNCHING IN
+          </p>
           <CustomCountdown days={2} hours={5} minutes={30} seconds={15} />
         </div>
       </div>
