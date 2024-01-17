8753f1a23585c01eb8ec23daf6a3c225559327cb
bhagirath-bhp <bhagirath.bhp@gmail.com>
2024-01-17 11:56:15 +0530

    Update

--- src/components/BurgerIcon.jsx
+++ src/components/BurgerIcon.jsx
@@ -10,20 +10,20 @@ const BurgerIcon = () => {
     return (
         <div
             id="nav-icon4"
-            className={`w-12 h-9 relative m-auto cursor-pointer transition-transform ${isOpen ? 'rotate-45 translate-y-[-3px] translate-x-8' : ''
+            className={`w-12 h-9 flex flex-col gap-[2rem] relative m-auto cursor-pointer transition-transform ${isOpen ? 'rotate-45 translate-y-[-3px] translate-x-8' : ''
                 }`}
             onClick={handleToggle}
         >
             <span
-                className={`block absolute h-9 bg-blue-600 rounded-full opacity-100 left-0 transition-transform ${isOpen ? 'rotate-45 translate-y-[-3px] translate-x-8' : ''
+                className={`block absolute h-1 w-9 bg-blue-600 rounded-full opacity-100 left-0 transition-transform ${isOpen ? 'rotate-45 translate-y-[-3px] translate-x-8' : ''
                     }`}
             ></span>
             <span
-                className={`block absolute h-9 bg-blue-600 rounded-full opacity-0 left-0 transition-opacity ${isOpen ? 'opacity-0' : ''
+                className={`block absolute h-1 w-9 bg-blue-600 rounded-full opacity-0 left-0 transition-opacity ${isOpen ? 'opacity-0' : ''
                     }`}
             ></span>
             <span
-                className={`block absolute h-9 bg-blue-600 rounded-full opacity-100 left-0 transition-transform ${isOpen ? '-rotate-45 translate-y-[39px] translate-x-8' : ''
+                className={`block absolute h-1 w-9 bg-blue-600 rounded-full opacity-100 left-0 transition-transform ${isOpen ? '-rotate-45 translate-y-[39px] translate-x-8' : ''
                     }`}
             ></span>
         </div>
