ff0f00062508187ebb3469d0108c2b57dd4bb0e2
bhagirath-bhp <bhagirath.bhp@gmail.com>
2024-01-29 21:46:25 +0530

    Timer fixed

--- src/components/CustomCountdown.jsx
+++ src/components/CustomCountdown.jsx
@@ -1,56 +1,43 @@
 import React, { useState, useEffect } from "react";
 import { Countdown } from "react-daisyui";
+import { calculateTimeRemaining } from "../utils";
 
 
-const CustomCountdown = ({ days, hours, minutes, seconds }) => {
-    const calculateRemainingTime = () => {
-        const totalSeconds = days * 24 * 60 * 60 + hours * 60 * 60 + minutes * 60 + seconds;
-        return totalSeconds;
-    };
+const CustomCountdown = () => {
 
-    const [remainingTime, setRemainingTime] = useState(calculateRemainingTime);
+    const targetTime = new Date('2024-02-08T00:00:00');
+    const [remainingTime, setRemainingTime] = useState(calculateTimeRemaining(targetTime));
 
     useEffect(() => {
-        const timer = setTimeout(() => {
-            setRemainingTime((prevTime) => (prevTime <= 0 ? 0 : prevTime - 1));
+        const timer = setInterval(() => {
+            setRemainingTime(calculateTimeRemaining(targetTime));
         }, 1000);
 
-        return () => {
-            clearTimeout(timer);
-        };
-    }, [remainingTime]);
-
-    const formatTime = (time) => {
-        return time < 10 ? `0${time}` : time;
-    };
-
-    const daysLeft = formatTime(Math.floor(remainingTime / (24 * 60 * 60)));
-    const hoursLeft = formatTime(Math.floor((remainingTime % (24 * 60 * 60)) / 3600));
-    const minutesLeft = formatTime(Math.floor((remainingTime % 3600) / 60));
-    const secondsLeft = formatTime(remainingTime % 60);
+        return () => clearInterval(timer);
+    }, [targetTime]);
     
 
     return (
         <div className="flex flex-col justify-center text-center font-metropolis">
             <div className="flex flex-col text-left relative">
                 {/* <Countdown className="font-mono text-[10rem]" value={daysLeft} /> */}
-                <span className="text-[7rem] font-[100]">{daysLeft}</span>
+                <span className="text-[7rem] font-[100]">{remainingTime.days}</span>
                 <span className="bg-blue-700 w-fit px-4 py-1 absolute right-[-0.6rem] bottom-[30%]">days</span>
             </div>
             <div className="hms flex gap-10">
                 <div className="flex flex-col">
                     {/* <Countdown className="font-mono text-5xl" value={hoursLeft} /> */}
-                    <span className="font-mono text-xl">{hoursLeft}</span>
+                    <span className="font-mono text-xl">{remainingTime.hours}</span>
                     hours
                 </div>
                 <div className="flex flex-col">
                     {/* <Countdown className="font-mono text-5xl" value={minutesLeft} /> */}
-                    <span className="font-mono text-xl">{minutesLeft}</span>
+                    <span className="font-mono text-xl">{remainingTime.minutes}</span>
                     min
                 </div>
                 <div className="flex flex-col">
                     {/* <Countdown className="font-mono text-5xl" value={secondsLeft} /> */}
-                    <span className="font-mono text-xl">{secondsLeft}</span>
+                    <span className="font-mono text-xl">{remainingTime.seconds}</span>
                     sec
                 </div>
             </div>
