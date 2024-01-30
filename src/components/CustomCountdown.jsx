c20683ec96aad85e438bfa9cc53455231440be17
Dev Parikh <51128342+Dev79844@users.noreply.github.com>
2024-01-30 21:52:18 +0530

    fix: changed font color

--- src/components/CustomCountdown.jsx
+++ src/components/CustomCountdown.jsx
@@ -21,24 +21,24 @@ const CustomCountdown = () => {
         <div className="flex flex-col justify-center text-center font-metropolis">
             <div className="flex flex-col text-left relative">
                 {/* <Countdown className="font-mono text-[10rem]" value={daysLeft} /> */}
-                <span className="cd-days text-[7rem] font-[100]">{remainingTime.days}</span>
+                <span className="cd-days text-[7rem] font-[100] text-white">{remainingTime.days}</span>
                 {/* <span className="bg-blue-700 w-fit px-4 py-1 absolute right-[-0.6rem] bottom-[30%]">days</span> */}
             </div>
             <div className="hms flex gap-10">
                 <div className="flex flex-col">
                     {/* <Countdown className="font-mono text-5xl" value={hoursLeft} /> */}
-                    <span className="font-mono text-xl">{remainingTime.hours}</span>
-                    hours
+                    <span className="font-mono text-xl text-white">{remainingTime.hours}</span>
+                    <span className="text-white">hours</span>
                 </div>
                 <div className="flex flex-col">
                     {/* <Countdown className="font-mono text-5xl" value={minutesLeft} /> */}
-                    <span className="font-mono text-xl">{remainingTime.minutes}</span>
-                    min
+                    <span className="font-mono text-xl text-white">{remainingTime.minutes}</span>
+                    <span className="text-white">min</span>
                 </div>
                 <div className="flex flex-col">
                     {/* <Countdown className="font-mono text-5xl" value={secondsLeft} /> */}
-                    <span className="font-mono text-xl">{remainingTime.seconds}</span>
-                    sec
+                    <span className="font-mono text-xl text-white">{remainingTime.seconds}</span>
+                    <span className="text-white">sec</span>
                 </div>
             </div>
         </div>
